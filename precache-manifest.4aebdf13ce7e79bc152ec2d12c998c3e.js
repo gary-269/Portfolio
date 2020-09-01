dbe449dc2ff93d40fe18e7fd7635ac9fce8bade4
gary-269 <karanveersingh269@gmail.com>
2020-09-01 01:01:34 -0400

    Updates

---
+++ precache-manifest.4aebdf13ce7e79bc152ec2d12c998c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32fdf172a09621a76087",
+    "url": "/Portfolio/static/css/main.2831e6ea.chunk.css"
+  },
+  {
+    "revision": "32fdf172a09621a76087",
+    "url": "/Portfolio/static/js/main.32fdf172.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "e850547311d8e16ce504dd710b337fdd",
+    "url": "/Portfolio/index.html"
+  }
+];
