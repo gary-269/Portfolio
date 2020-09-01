f1d1e2abecdfa33cf4aaf9a0c20136ce37531452
gary-269 <karanveersingh269@gmail.com>
2020-09-01 00:41:55 -0400

    Files added

---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+    "imagebaseurl":"https://gary-269.github.io/",
+    "name": "Karanveer Singh",
+    "role": "Web Developer and Data Science Enthusiast",
+    "linkedinId":"linkedin.com/in/karanveer-singh-599303156/",
+    "roleDescription": " Ambitious, hard-working, dedicated- these are the words that define me as an individual. I am keen to learn new technologies and keep up to date with the current market. Always up for new challenges, I push myself to grow as a person- both professionally and personally. I love web development and Data Science.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/karanveer-singh-599303156/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/gary-269",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/Karanve11298114",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a Computer Science Graduate student at University of Windsor. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I completed my B.Tech in Computer Science and Engineering form Punjabi University Patiala. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Canada",
+    "website":"https://github.com/gary-269/gary-269.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Windsor",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Punjabi University Patiala",
+        "specialization":"Computer Science and Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Ducat Education",
+        "specialization":"Web Development",
+        "MonthOfLeaving":"Jan - July",
+        "YearOfLeaving":"2018",
+        "Achievements":"Developed an E-commerce website using HTML, CSS, JavaScript, PHP and MySQL"
+      },
+      {
+        "CompanyName":"SlickSoft Technologies",
+        "specialization":"Web Development",
+        "MonthOfLeaving":"June - July",
+        "YearOfLeaving":"2016",
+        "Achievements":"Developed a TechFest Management System using Java and MySQL."
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"Flask"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Sentiment Analyser",
+        "description":"Sentiment Analysis using Machine Learning",
+        "imgurl":"images/portfolio/sentiment.jpg",
+      },
+      {
+        "name":"Phone Store",
+        "description":"An E-commerce website for Mobile Phones created using React",
+        "imgurl":"images/portfolio/phonestore.jpg"
+      },
+      {
+        "name":"Food Ordering App",
+        "description":"A web application for ordering food online created using Django",  
+        "imgurl":"images/portfolio/food.jpg"
+      },
+      {
+        "name":"Search Engine",
+        "description":"Web search engine created using Java",
+        "imgurl":"images/portfolio/search.png"
+      },
+      {
+        "name":"Real Estate Price Predictor",
+        "description":"Web application for predicting the price of houses using machine learning",
+        "imgurl":"images/portfolio/real.png"
+      },
+      {
+        "name":"Multiplayer game",
+        "description":"Multiplayer game created using C",
+        "imgurl":"images/portfolio/game.png"
+      },
+    ],
+  }
+  
+  export default resumeData
